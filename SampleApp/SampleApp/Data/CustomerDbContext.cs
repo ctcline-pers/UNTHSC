7d830c784afd0a122b39ece8741e915ae66925fa
Cristina T. Cline <cristinatcline@gmail.com>
2019-09-02 22:57:57 -0500

    Forgot to switch to develop...my bad

---
+++ SampleApp/SampleApp/Data/CustomerDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleApp.Data
+{
+    // public class CustomerDbContext : DbContext
+    // {
+    //     public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+    //     :base(options)
+    //     {
+    //     }
+
+    //     public DbSet<CustomerDbContext> Customer {get; set;}
+
+    // }
+}
