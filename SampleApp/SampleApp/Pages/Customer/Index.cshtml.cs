847f8ae14fa0c664e1a3e9e4b91400bd41e1bca2
Cristina T. Cline <cristinatcline@gmail.com>
2019-09-03 10:00:20 -0500

    Fixed sort and filter bug

--- SampleApp/SampleApp/Pages/Customer/Index.cshtml.cs
+++ SampleApp/SampleApp/Pages/Customer/Index.cshtml.cs
@@ -34,56 +34,89 @@ namespace SampleApp.Pages_Customer
         public string CountryFilter {get; set;}
         public string EmailFilter {get; set;}
 
-        public async Task OnGetAsync(bool? clearSort, bool? clearSearch, string firstOrder, string lastOrder, string nameSearch, string zipSearch,string countrySearch, string emailSearch, int? pageIndex)
+        public async Task OnGetAsync(bool? clearSort, bool? clearSearch, string firstOrder, string lastOrder, string dobOrder, string createdOrder, string nameSearch, string zipSearch,string countrySearch, string emailSearch, int? pageIndex)
         {
             var orderParam = "";
 
-            //Set all of the current sorts to empty strings to exclude order by clause
-            if(clearSort != null && clearSort.Value)
-            {
-                CurrentFirstSort = "";
-                CurrentLastSort = "";
-                CurrentDOBSort = "";
-                CurrentCreateDateSort = "";
-                
-            }
-            else
-            {
-                //If current sort is ascending, switch to descending. If not currently sorted, default to ascending
-                if(!string.IsNullOrEmpty(firstOrder))
+                if(clearSort != null && clearSort.Value)
                 {
-                    CurrentFirstSort = firstOrder == "FirstName ASC" ? "FirstName DESC" : "FirstName ASC";                    
+                    CurrentFirstSort = "";
+                    CurrentLastSort = "";
+                    CurrentDOBSort = "";
+                    CurrentCreateDateSort = "";
+                    
                 }
-
-                if(!string.IsNullOrEmpty(lastOrder))
+                else
                 {
+                    //If current sort is ascending, switch to descending. If not currently sorted, default to ascending
+                    CurrentFirstSort = firstOrder == "FirstName ASC" ? "FirstName DESC" : "FirstName ASC";                 
+                    
                     CurrentLastSort = lastOrder == "LastName ASC" ? "LastName DESC" : "LastName ASC";
+                    
+                    CurrentDOBSort = dobOrder == "DateOfBirth ASC" ? "DateOfBirth DESC" : "DateOfBirth ASC";
+
+                    CurrentCreateDateSort = createdOrder == "CreatedDate ASC" ? "CreatedDate DESC" : "CreatedDate ASC";
+                    
+                    //Prioritize the sort order based on the link clicked
+                    if(!string.IsNullOrEmpty(firstOrder))
+                    {
+                        orderParam = $"{(string.IsNullOrEmpty(CurrentFirstSort) ? "" : CurrentFirstSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentLastSort) ? "" : CurrentLastSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentDOBSort) ? "" : CurrentDOBSort + ",")}" +
+                        $"{(string.IsNullOrEmpty(CurrentCreateDateSort) ? "" : CurrentCreateDateSort+ ",")}";
+                    }
+
+                    if(!string.IsNullOrEmpty(lastOrder))
+                    {
+                        orderParam = $"{(string.IsNullOrEmpty(CurrentLastSort) ? "" : CurrentLastSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentFirstSort) ? "" : CurrentFirstSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentDOBSort) ? "" : CurrentDOBSort + ",")}" +
+                        $"{(string.IsNullOrEmpty(CurrentCreateDateSort) ? "" : CurrentCreateDateSort+ ",")}";
+                    }
+
+                    if(!string.IsNullOrEmpty(dobOrder))
+                    {
+                        orderParam = $"{(string.IsNullOrEmpty(CurrentDOBSort) ? "" : CurrentDOBSort + ",")}" +
+                        $"{(string.IsNullOrEmpty(CurrentFirstSort) ? "" : CurrentFirstSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentLastSort) ? "" : CurrentLastSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentCreateDateSort) ? "" : CurrentCreateDateSort+ ",")}";
+                    }
+
+                    if(!string.IsNullOrEmpty(createdOrder))
+                    {
+                        orderParam = $"{(string.IsNullOrEmpty(CurrentCreateDateSort) ? "" : CurrentCreateDateSort+ ",")}"+
+                        $"{(string.IsNullOrEmpty(CurrentFirstSort) ? "" : CurrentFirstSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentLastSort) ? "" : CurrentLastSort+ ",")}" + 
+                        $"{(string.IsNullOrEmpty(CurrentDOBSort) ? "" : CurrentDOBSort + ",")}" ;
+                    }
+                    
+                }   
+
+                //If clearing search, set search fields to empty and start over at page one after data refresh
+                if(clearSearch != null && clearSearch.Value)
+                {
+                    pageIndex = 1;
+                    CountryFilter = "";
+                    NameFilter = "";
+                    ZipFilter = "";
+                    EmailFilter = "";
                 }
-                
-                   
-                //Builds the order by clause to pass to the sproc
-                orderParam = $"{(string.IsNullOrEmpty(CurrentFirstSort) ? "" : CurrentFirstSort+ ",")}" + 
-                $"{(string.IsNullOrEmpty(CurrentLastSort) ? "" : CurrentLastSort+ ",")}" + $"{(string.IsNullOrEmpty(CurrentDOBSort) ? "" : CurrentDOBSort + ",")}" +
-                $"{(string.IsNullOrEmpty(CurrentCreateDateSort) ? "" : CurrentCreateDateSort+ ",")}";
-            }   
+                else
+                {
+                    NameFilter = nameSearch;
+                    EmailFilter = emailSearch;
+                    ZipFilter = zipSearch;
+                    CountryFilter = countrySearch;
+                }         
 
-            //If clearing search, set search fields to empty and start over at page one after data refresh
-            if(clearSearch != null && clearSearch.Value)
-            {
-                pageIndex = 1;
-                CountryFilter = "";
-                NameFilter = "";
-                ZipFilter = "";
-                EmailFilter = "";
-            }         
-
-            //If data is currently being sorted, clean up parameter and reset page to 1
-            if(!string.IsNullOrEmpty(orderParam))
-            {
-                orderParam.Remove(orderParam.Length - 1);
-                orderParam = $"ORDER BY {orderParam}";
-                pageIndex = 1;
-            }
+                //If data is currently being sorted, clean up parameter and reset page to 1
+                if(!string.IsNullOrEmpty(orderParam))
+                {
+                    orderParam = orderParam.Remove(orderParam.Length - 1);
+                    orderParam = $"ORDER BY {orderParam}";
+                    pageIndex = 1;
+                }
+           
 
             int pageSize = 5;
 
